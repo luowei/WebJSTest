57b8057e3121ac47b8a21c1548b5f71fd50e61a9
luowei <luowei@wodedata.com>
2018-09-10 21:28:32 +0800

    Initial Commit

---
+++ WebJSTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebJSTest
+//
+//  Created by luowei on 2018/9/10.
+//  Copyright © 2018年 luowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
